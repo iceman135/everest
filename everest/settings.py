c6522c740b67170979d2a2b5e68b116c4e7d3740
iceman135 <deepaks135@gmail.com>
2021-02-23 11:09:27 +0530

    added configs in settings

--- everest/settings.py
+++ everest/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dcelery'
+    'dcelery',
+    'django_jenkins'
 ]
+
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount'
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
